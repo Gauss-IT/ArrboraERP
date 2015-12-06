f2eca8af158b8272c8439b431ce1b4ef8f80891a
Admir Huseini <ad.huseini@gmail.com>
2015-12-06 02:28:07 +0100

    Changes in the data alyer

--- Arrbora.Data/DataModel/ProductDataModel.cs
+++ Arrbora.Data/DataModel/ProductDataModel.cs
@@ -18,26 +18,26 @@ namespace Arrbora.Data.DataModel
         /// <summary>
         /// Gets or sets price id
         /// </summary>
-        int PriceID { get; set; }
+        public int PriceID { get; set; }
 
         /// <summary>
         /// Gets or sets price
         /// </summary>
-        int Price { get; set; }
+        public int Price { get; set; }
         
         /// <summary>
         /// Gets or sets transport costs
         /// </summary>
-        int Transport { get; set; }
+        public int Transport { get; set; }
 
         /// <summary>
         /// Gets or sets other costs
         /// </summary>
-        int Other1 { get; set; }
+        public int Other1 { get; set; }
 
         /// <summary>
         /// Gets or sets secondary other costs
         /// </summary>
-        int Other2 { get; set; }
+        public int Other2 { get; set; }
     }
 }
